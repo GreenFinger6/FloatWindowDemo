48ed5dcf67e00d66602a1cd5b1460eba658467dc
xiaofang <xiaofang0503@qq.com>
2026-07-14 15:37:18 +0800

    配置项现在以功能为类别组织

--- app/src/main/java/com/example/floatwindowdemo/utils/GameConfig.kt
+++ app/src/main/java/com/example/floatwindowdemo/utils/GameConfig.kt
@@ -3,15 +3,11 @@ package com.example.floatwindowdemo.utils
 /**
  * 统一管理游戏静态常量( 1237*720 分辨率下)
  */
-object GameConfig {
 
+// 拍卖行相关常量
+object Auction {
     // 1. 按钮中心坐标 (x, y)
     object Buttons {
-        val START = Pair(540f, 1200f)
-        val RETRY = Pair(300f, 800f)
-        val CLOSE = Pair(1000f, 50f)
-
-        // 拍卖行抢拍相关按钮
         val PaiMaiHang = Pair(0.4964f, 0.2306f) // 拍卖行第一个物品位置
         val PaiMaiHang2 = Pair(0.0534f, 0.5764f) // 返回拍卖行
         val PaiMaiHang3 = Pair(66f, 415f) // 购买
@@ -31,12 +27,11 @@ object GameConfig {
         val MIN_PRICE = RectArea(0.1148f, 0.3708f, 0.4770f, 0.4556f)
     }
 
-    // 3. 文本常量
-    object Texts {
-        const val VICTORY = "胜利"
-        const val DEFEAT = "失败"
-        const val LOADING = "加载中"
-    }
+    // 涉及的相关模版
+    val templateList = listOf(
+        "state_auction_purchase", //判断购买页面
+        "state_auction_detail"  //判断商品详情
+    )
 }
 
 // 定义一个简单的区域数据类
